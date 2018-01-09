ae12f6f3ca8f8f304dfe7be638085494eaaaf3c5
Trav Colbert <trav.colbert@gmail.com>
2018-01-08 16:01:21 -0800

    new client-side element-fetcher

---
+++ public/js/authUI.js
@@ -0,0 +1,55 @@
+let getAuthElements = function() {
+  return document.getElementsByClassName("authUI");
+}
+
+let authUI = function(list) {
+  for(let c=0;c<list.length;c++) {
+    console.log(list[c].dataset.fetch);
+    requestElement(list[c]);
+  }
+}
+
+let requestElement = function(parentElement) {
+  // .dataset.fetch
+  let requestObj = {
+    method:'POST',
+    credentials:'include'
+  };
+  if(parentElement.dataset.hasOwnProperty('params')) requestObj.body = JSON.stringify({params:parentElement.dataset.params});
+  fetch('/authorizedelements/' + parentElement.dataset.fetch, requestObj).
+  then(function(response) {
+    return response.json();
+  }).
+  then(function(elementJson) {
+    if(elementJson.hasOwnProperty('error')) {
+      console.log(elementJson.error);
+    } else {
+      console.log(elementJson);
+      if(Array.isArray(elementJson)) {
+        let requestedElement = [];
+        for(let c=0;c<elementJson.length;c++) {
+          elementJson[c].parent = parentElement;
+          requestedElement[c] = new Laminar.Widget(elementJson);
+        }
+      } else {
+        elementJson.parent = parentElement;
+        let user_button = new Laminar.Widget(elementJson);
+      }
+    }
+  })
+}
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+  // Make a list of components that need to be authorized before enabling
+  // Components will probably be a placeholder DIV
+  let listOfElements = getAuthElements();
+  console.log(listOfElements.length);
+  authUI(listOfElements);
+});
